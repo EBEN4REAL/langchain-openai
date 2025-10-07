77e21835dda77663752defcce3955082e0b8452d
Ebenezer Igbinoba <ebenezer.igbinoba@stu.cu.edu.ng>
2025-10-07 03:16:52 +0100

    update prompts file

--- facts/prompt.py
+++ facts/prompt.py
@@ -6,9 +6,13 @@ from langchain.chains import RetrievalQA
 from langchain_community.document_loaders import TextLoader
 from langchain_openai import ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from redundant_filter_retriever import RedundantFilterRetriever
 import os
 
 load_dotenv()
+
+# 5️⃣ Initialize your LLM
+llm = ChatOpenAI(temperature=0.3)
 
 # 1️⃣ Load your document
 current_dir = os.path.dirname(__file__)
@@ -31,20 +35,28 @@ db = Chroma.from_documents(
     persist_directory="./facts/facts_chroma_db"
 )
 
-retriever = db.as_retriever(search_kwargs={"k": 1})  # retrieve top 1 chunks
+retriever = db.as_retriever()  # retrieve top 1 chunks
 
-# 5️⃣ Initialize your LLM
-llm = ChatOpenAI(temperature=0.3)
+# Create your custom retriever
+retriever = RedundantFilterRetriever(
+    vectorstore=db,
+    threshold=0.8,      # Similarity threshold for filtering
+    k=5,                # Number of documents to return
+    fetch_k=20,         # Number of candidates to fetch for MMR
+    lambda_mult=0.5     # Balance between relevance (1.0) and diversity (0.0)
+)
 
 # 6️⃣ Build the RetrievalQA chain
 qa = RetrievalQA.from_chain_type(
     llm=llm,
-    chain_type="stuff",     # “Here are 3 text chunks → LLM, answer using them all.”
-    retriever=retriever,
+    chain_type="map_reduce",  # The LLM reads each chunk individually and summarizes or answers the question for that chunk, The partial answers/summaries are merged and summarized again into a final answer.
+    # chain_type="stuff",     # “Here are 3 text chunks → LLM, answer using them all.”
+    retriever=retriever, 
     return_source_documents=True,  # optional, to see what context was used (This is optional but very useful for debugging and explainability.)
 )
 
 result = qa.invoke({"query": "What is an interesting fact about the English Language?"})
+
 
 print(result["result"]) # the answer
 print(len(result["source_documents"]))      # the chunks retrieved from your DB
